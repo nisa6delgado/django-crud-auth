349fcf3494c77929ffd2570e7bb2f3d8ed69f03b
nisa6delgado <nisadelgado@gmail.com>
2025-08-11 11:10:49 -0400

    Estilo en formularios

--- tasks/forms.py
+++ tasks/forms.py
@@ -11,11 +11,20 @@ class TaskForm(forms.ModelForm):
             'important'
         ]
 
+        labels = {
+            'title': 'Título',
+            'description': 'Descripción',
+            'important': 'Importante',
+        }
+
         widgets = {
             'title': forms.TextInput(
-                attrs={ 'class': 'w-full block p-2 border border-gray-300 rounded' }
+                attrs={ 'class': 'w-full block p-2 border border-gray-300 rounded mb-5' }
             ),
             'description': forms.Textarea(
-                attrs={ 'class': 'w-full block p-2 border border-gray-300 rounded' }
+                attrs={ 'class': 'w-full block p-2 border border-gray-300 rounded mb-5' }
+            ),
+            'important': forms.CheckboxInput(
+                attrs={ 'class': 'mb-5' }
             ),
         }
